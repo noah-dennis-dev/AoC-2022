07697d0ecb55c8bc7f85c8d646a999dcad238e3e
Noah Dennis <noahwd123@icloud.com>
2022-12-01 05:28:01 +0000

    Day 1

--- day01.js
+++ day01.js
@@ -1,10 +1,16 @@
 const fs = require('fs');
-const lines = fs.readFileSync('./input.txt', 'utf8').split('\n');
-
+const lines = fs.readFileSync('./input.txt', 'utf8').split('\n\n');
 
 function Part1() {
-
-
+    let largest = 0;
+    for (let i = 0; i < lines.length; i++) {
+        let count = 0;
+        for (let item of lines[i].split('\n')) {
+            count += parseInt(item);
+        }
+        if (count > largest) largest = count;
+    }
+    console.log(largest);
 }
 
 Part1();
@@ -12,8 +18,22 @@ Part1();
 
 
 function Part2() {
+    let items = [];
+    for (let i = 0; i < lines.length; i++) {
+        let count = 0;
+        for (let item of lines[i].split('\n')) {
+            count += parseInt(item);
+        }
+        items.push(count);
+    }
 
+    items.sort();
+    let total = 0;
+    for (let i = items.length - 3; i < items.length; i++) {
+        total += items[i];
+    }
 
+    console.log(total);
 }
 
 Part2();
