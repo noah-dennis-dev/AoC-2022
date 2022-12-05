0b51c34cf12f313766aaaabc070d4e9f968e96cf
Noah Dennis <noahwd123@icloud.com>
2022-12-05 06:22:30 +0000

    Day 5

--- day05.js
+++ day05.js
@@ -1,19 +1,42 @@
 const fs = require('fs');
-let lines = fs.readFileSync('./input.txt', 'utf8').split('\n');
+let parts = fs.readFileSync('./input.txt', 'utf8').split('\n\n')
 
 
-function Part1() {
+let crates = parts[0].split('\n');
+let instructions = parts[1].split('\n');
+
+function ParseInput() {
+    let rows = [[], [], [], [], [], [], [], [], []];
+    crates.forEach(row => {
+
+        for (let i = 0; i < row.split(' ').length; i++) {
+            if (row.split(' ')[i] !== '.') {
+                rows[i].push(row.split(' ')[i])
+            }
+        }
 
 
+    });
+    return rows
 }
 
-Part1();
 
+let crateRows = ParseInput();
 
+instructions.forEach(instruction => {
+    let parts = instruction.split(',').map(el => parseInt(el));
+    let addingItems = [];
+    for (let i = 0; i < parts[0]; i++) {
+        addingItems.push(crateRows[parts[1] - 1].shift());
+    }
+    addingItems.reverse();
+    addingItems.forEach(item => {
+        crateRows[parts[2] - 1].unshift(item);
+    })
 
-function Part2() {
+    console.log(crateRows)
+})
 
-
-}
-
-Part2();
+crateRows.forEach(row => {
+    console.log(row[0])
+})
