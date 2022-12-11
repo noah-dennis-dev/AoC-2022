3f8047ab1ec6a2cc2c273817db67aa9afa3f381c
Noah Dennis <noahwd123@icloud.com>
2022-12-11 21:22:28 +0000

    Day 11

--- day10.js
+++ day10.js
@@ -1,19 +1,49 @@
 const fs = require('fs');
-let lines = fs.readFileSync('./input.txt', 'utf8').split('\n');
+let lines = fs.readFileSync('./test.txt', 'utf8').split('\n');
+
+let cycles = 0;
+let x = 1;
+let count = 0;
+let checked = [];
+let crt = 0;
+
+let output = "";
+lines.forEach(line => {
+    if (line === 'noop') {
+        cycles++;
+        crt = (crt + 1) % 40;
+        CheckCycles()
+    } else {
+        let parts = line.split(' ');
+        for (let i = 0; i < 2; i++) {
+            cycles++;
+            crt = (crt + 1) % 40;
+            if (crt >= x - 1 && crt <= x + 1) {
+                output += '#';
+            } else {
+                output += '.';
+            }
+            CheckCycles();
+
+            console.log(crt, x, crt >= x - 1 && crt <= x + 1)
+        }
+
+        x += parseInt(parts[1]);
+    }
+})
 
 
-function Part1() {
-
-
+for (let current = 0; current < output.length / 40; current++) {
+    console.log(output.substring(current * 40, (current + 1) * 40))
 }
 
-Part1();
 
 
-
-function Part2() {
-
-
+function CheckCycles() {
+    if (cycles == 20 || cycles == 60 || cycles == 100 || cycles == 140 || cycles == 180 | cycles == 220) {
+        if (!checked.includes(cycles)) {
+            count += (cycles * x);
+            checked.push(cycles);
+        }
+    }
 }
-
-Part2();
--- day11.js
+++ day11.js
@@ -1,19 +1,54 @@
 const fs = require('fs');
-let lines = fs.readFileSync('./input.txt', 'utf8').split('\n');
+let monkeys = fs.readFileSync('./input.txt', 'utf8').split('\n\n');
+
+let monkeyVals = [];
+
+monkeys.forEach(monkey => {
+    let lines = monkey.split('\n');
+
+    let i = parseInt(lines[0].replace('Monkey ', '').replace(':', ''));
+    let startingItems = lines[1].replace('  Starting items: ', '').split(', ').map(el => parseInt(el));
+    let operation = lines[2].replace('  Operation: new = ', '').split(' ');
+    let test = parseInt(lines[3].replace('  Test: divisible by ', ''));
+    let truePass = parseInt(lines[4].replace('    If true: throw to monkey ', ''));
+    let falsePass = parseInt(lines[5].replace('    If false: throw to monkey ', ''));
+
+    monkeyVals.push({ i, startingItems, operation, test, truePass, falsePass, inspected: 0 })
+})
 
 
-function Part1() {
-
-
+let maxStress = 1;
+for (let monkey of monkeyVals) {
+    maxStress *= monkey.test;
 }
 
-Part1();
+for (let i = 0; i < 10000; i++) {
+    monkeyVals.forEach(monkey => {
+        for (item of monkey.startingItems) {
+            if (monkey.operation[1] === '*') {
+                item = item * (Number(monkey.operation[2]) || item);
+            } else {
+                item = item + Number(monkey.operation[2] || item);
+            }
 
+            item = item % maxStress;
 
+            if (item % monkey.test === 0) {
+                monkeyVals[monkey.truePass].startingItems.push(item);
+            } else {
+                monkeyVals[monkey.falsePass].startingItems.push(item);
+            }
 
-function Part2() {
-
-
+            monkey.inspected += monkey.startingItems.length
+            monkey.startingItems = []
+        }
+    })
 }
 
-Part2();
+console.log(monkeyVals);
+monkeyVals.sort((a, b) => a.inspected - b.inspected)
+
+let vals = [monkeyVals.pop().inspected, monkeyVals.pop().inspected]
+
+console.log(vals);
+console.log(vals.reduce((prev, curr) => prev * curr));
