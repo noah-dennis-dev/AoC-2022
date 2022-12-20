d9c7962ed7d76e5dae75f3d1ae3f8335e7483edc
Noah Dennis <noahwd123@icloud.com>
2022-12-20 05:52:31 +0000

    Day 20

--- day20.js
+++ day20.js
@@ -1,19 +1,18 @@
 const fs = require('fs');
-let lines = fs.readFileSync('./input.txt', 'utf8').split('\n');
+let items = fs.readFileSync('./input.txt', 'utf8').split('\n').map(Number);
+
+const nums = items.map(x => parseInt(x));
+const key = 811589153;
+const list = nums.map((val, i) => ({ val: val * key, i }));
 
 
-function Part1() {
-
-
+for (let x = 0; x < list.length * 10; x++) {
+    const currentInd = list.findIndex(({ i }) => i === x % list.length);
+    const [el] = list.splice(currentInd, 1);
+    list.splice((currentInd + el.val) % list.length, 0, el);
 }
 
-Part1();
 
-
-
-function Part2() {
-
-
-}
-
-Part2();
+const zeroIndex = list.findIndex(el => el.val === 0);
+const keys = [1000, 2000, 3000].map(x => list[(zeroIndex + x) % list.length].val);
+console.log(keys.reduce((prev, curr) => prev + curr));
