b847b1e384f522ece1d4ecb692cbce7d1f481d25
Noah Dennis <noahwd123@icloud.com>
2022-12-21 06:04:51 +0000

    Day 21

--- day21.js
+++ day21.js
@@ -1,19 +1,50 @@
 const fs = require('fs');
 let lines = fs.readFileSync('./input.txt', 'utf8').split('\n');
 
+let monkeys = {};
 
-function Part1() {
-
-
+for (let line of lines) {
+    let monkey = line.split(':')[0];
+    let mathsOp = isNaN(line.split(': ')[1])
+    let RHS = line.split(': ')[1];
+    if (!mathsOp) { RHS = Number(RHS); }
+    else { RHS = RHS.split(' ') }
+    monkeys[monkey] = ({ mathsOp, RHS })
 }
 
-Part1();
+// Did this part manually changing the initial value of i, after comparing the difference.
+for (let i = 3678125400000; i < 10000000000000; i++) {
+    monkeys['humn']['RHS'] = i;
 
+    let total = FindTotal('root');
 
-
-function Part2() {
-
-
+    if (total === true) {
+        console.log(i);
+        break;
+    }
 }
 
-Part2();
+
+
+function FindTotal(name) {
+    if (name == 'root') {
+        let item1 = FindTotal(monkeys[name]['RHS'][0]);
+        let item2 = FindTotal(monkeys[name]['RHS'][2]);
+        console.log(item1 - item2)
+        if (item1 === item2) {
+            return true;
+        }
+    }
+
+    if (!isNaN(monkeys[name]['RHS'])) return monkeys[name]['RHS'];
+
+    let item1 = FindTotal(monkeys[name]['RHS'][0]);
+    let item2 = FindTotal(monkeys[name]['RHS'][2]);
+
+    let operator = monkeys[name]['RHS'][1];
+
+    if (operator === '+') return item1 + item2;
+    if (operator === '-') return item1 - item2;
+    if (operator === '/') return item1 / item2;
+    if (operator === '*') return item1 * item2;
+}
